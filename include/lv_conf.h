47dbc97434f700ed011bf928c6f5ebe9218e61a8
mverch67 <manuel.verch@gmx.de>
2024-04-18 14:41:43 +0100

    make MEM/PERF OCD configurable

--- include/lv_conf.h
+++ include/lv_conf.h
@@ -289,14 +289,18 @@
  *-----------*/
 
 /*1: Show CPU usage and FPS count*/
-#define LV_USE_PERF_MONITOR 1
+#ifndef LV_USE_PERF_MONITOR
+#define LV_USE_PERF_MONITOR 0
+#endif
 #if LV_USE_PERF_MONITOR
     #define LV_USE_PERF_MONITOR_POS LV_ALIGN_BOTTOM_RIGHT
 #endif
 
 /*1: Show the used memory and the memory fragmentation
  * Requires LV_MEM_CUSTOM = 0*/
-#define LV_USE_MEM_MONITOR 1
+#ifndef LV_USE_MEM_MONITOR
+#define LV_USE_MEM_MONITOR 0
+#endif
 #if LV_USE_MEM_MONITOR
     #define LV_USE_MEM_MONITOR_POS LV_ALIGN_BOTTOM_LEFT
 #endif
