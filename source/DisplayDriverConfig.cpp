ff3dd691bed2afbc3ee34f90ccbb52f1badbfdaa
mverch67 <manuel.verch@gmx.de>
2024-05-09 10:35:59 +0200

    X11 panel dimensions

--- source/DisplayDriverConfig.cpp
+++ source/DisplayDriverConfig.cpp
@@ -1,8 +1,9 @@
 #include "DisplayDriverConfig.h"
 
-DisplayDriverConfig::DisplayDriverConfig(void) : _device(device_t::NONE) {}
+DisplayDriverConfig::DisplayDriverConfig(void) : _device(device_t::NONE), _width(c_default_width), _height(c_default_height) {}
 
-DisplayDriverConfig::DisplayDriverConfig(enum device_t device) : _device(device) {}
+DisplayDriverConfig::DisplayDriverConfig(enum device_t device, uint16_t width, uint16_t height) :
+                                             _device(device), _width(width), _height(height) {}
 
 DisplayDriverConfig::DisplayDriverConfig(struct panel_config_t &&panel, struct bus_config_t &&bus, struct light_config_t &&light,
                                          struct touch_config_t &&touch)
