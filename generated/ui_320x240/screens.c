6404761c3fad329fbd37738bacb0c75116b979a1
mverch67 <manuel.verch@gmx.de>
2024-10-22 17:44:05 +0200

    fix empty language

--- generated/ui_320x240/screens.c
+++ generated/ui_320x240/screens.c
@@ -3628,7 +3628,7 @@ void create_screen_main_screen() {
                     objects.settings_language_dropdown = obj;
                     lv_obj_set_pos(obj, 0, 0);
                     lv_obj_set_size(obj, 150, 30);
-                    lv_dropdown_set_options(obj, "English\nDeutsch\nEspañol\nFrançais\nItaliano\nNederlands\nPolski\nPortuguês\nSuomalainen\nSvenska\nTürkçe\n");
+                    lv_dropdown_set_options(obj, "English\nDeutsch\nEspañol\nFrançais\nItaliano\nNederlands\nPolski\nPortuguês\nSuomalainen\nSvenska\nTürkçe");
                     add_style_drop_down_style(obj);
                     lv_obj_set_style_align(obj, LV_ALIGN_TOP_MID, LV_PART_MAIN | LV_STATE_DEFAULT);
                     lv_obj_set_style_border_color(obj, lv_color_hex(0xffe0e0e0), LV_PART_MAIN | LV_STATE_DEFAULT);
