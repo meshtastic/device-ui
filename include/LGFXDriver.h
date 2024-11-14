f6d2d11a4acb3d0a873e1ee9eeb180c1294536bf
mverch67 <manuel.verch@gmx.de>
2024-11-14 08:49:15 +0000

    fix debug info set resolution

--- include/LGFXDriver.h
+++ include/LGFXDriver.h
@@ -258,8 +258,8 @@ template <class LGFX> void LGFXDriver<LGFX>::init(DeviceGUI *gui)
 
     lv_display_set_flush_cb(this->display, LGFXDriver::display_flush);
 
-    ILOG_DEBUG("Set display resolution: %dx%d", lgfx->screenWidth, lgfx->screenHeight);
 #if defined(DISPLAY_SET_RESOLUTION)
+    ILOG_DEBUG("Set display resolution: %dx%d", lgfx->screenWidth, lgfx->screenHeight);
     lv_display_set_resolution(this->display, lgfx->screenWidth, lgfx->screenHeight);
 #endif
     // lv_display_set_physical_resolution(this->display, this->screenWidth, this->screenHeight);
