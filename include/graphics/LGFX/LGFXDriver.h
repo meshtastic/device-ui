06c55a17abb465edeff52dcaad760c367ea4d43f
mverch67 <manuel.verch@gmx.de>
2025-02-11 23:52:05 +0100

    remove touch driver sleep/wakeup which may cause trouble on devices without touch reset; add also short delay to let cpu restore registers

--- include/graphics/LGFX/LGFXDriver.h
+++ include/graphics/LGFX/LGFXDriver.h
@@ -100,7 +100,6 @@ template <class LGFX> void LGFXDriver<LGFX>::task_handler(void)
                         DisplayDriver::view->screenSaving(true);
                         if (hasTouch() && hasButton()) {
                             ILOG_DEBUG("disable touch, enable button input");
-                            lgfx->touch()->sleep();
                             lv_indev_enable(DisplayDriver::touch, false);
                             lv_indev_enable(InputDriver::instance()->getButton(), true);
                         }
@@ -125,6 +124,7 @@ template <class LGFX> void LGFXDriver<LGFX>::task_handler(void)
                     }
                     if ((pin_int >= 0 && DisplayDriver::view->sleep(pin_int)) ||
                         (screenTimeout > lv_display_get_inactive_time(NULL) && !DisplayDriver::view->isScreenLocked())) {
+                        delay(2); // let the CPU finish to restore all register in case of light sleep
                         // woke up by touch or button
                         ILOG_INFO("leaving powersave");
                         powerSaving = false;
@@ -135,7 +135,6 @@ template <class LGFX> void LGFXDriver<LGFX>::task_handler(void)
                         DisplayDriver::view->screenSaving(false);
                         if (hasTouch() && hasButton()) {
                             ILOG_DEBUG("enable touch, disable button input");
-                            lgfx->touch()->wakeup();
                             lv_indev_enable(DisplayDriver::touch, true);
                             lv_indev_enable(InputDriver::instance()->getButton(), false);
                         }
