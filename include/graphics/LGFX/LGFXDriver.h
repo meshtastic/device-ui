c17ff458fee304f978538239dcc420d58b41fe59
Manuel <71137295+mverch67@users.noreply.github.com>
2025-03-24 20:11:10 +0100

    increase cache size and screen buffer (#104)

--- include/graphics/LGFX/LGFXDriver.h
+++ include/graphics/LGFX/LGFXDriver.h
@@ -254,7 +254,7 @@ template <class LGFX> void LGFXDriver<LGFX>::init(DeviceGUI *gui)
     assert(buf1 != 0);
     lv_display_set_buffers(display, buf1, buf2, bufsize, LV_DISPLAY_RENDER_MODE_PARTIAL);
 #else
-    bufsize = lgfx->screenWidth * lgfx->screenHeight / 2;
+    bufsize = lgfx->screenWidth * lgfx->screenHeight;
     buf1 = new lv_color_t[bufsize];
     assert(buf1 != 0);
     ILOG_DEBUG("LVGL: allocating %u bytes heap memory for draw buffer", sizeof(lv_color_t) * bufsize);
