95d7323be7a7aea45936bc3079fcc1a96c357258
mverch67 <manuel.verch@gmx.de>
2025-05-13 16:54:30 +0200

    inherit from SerialClient

--- include/comms/EthClient.h
+++ include/comms/EthClient.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "Client.h"
-#include "comms/IClientBase.h"
+#include "comms/SerialClient.h"
 #include "util/SharedQueue.h"
 #include <stdint.h>
 
@@ -9,7 +9,7 @@
 #define SERVER_PORT 4403
 #endif
 
-class EthClient : public IClientBase
+class EthClient : public SerialClient
 {
   public:
     EthClient(const char *serverName = "localhost", uint16_t port = SERVER_PORT);
@@ -17,38 +17,19 @@ class EthClient : public IClientBase
     EthClient(uint8_t *mac, IPAddress ip, IPAddress server = IPAddress(127, 0, 0, 1), uint16_t port = SERVER_PORT);
     // EthClient(uint8_t *mac, IPAddress ip, IPAddress server, IPAddress gateway, IPAddress subnet);
     void init(void) override;
-    bool sleep(int16_t pin);
     bool connect(void) override;
     bool disconnect(void) override;
     bool isConnected(void) override;
-    bool send(meshtastic_ToRadio &&to) override;
+    //    bool send(meshtastic_ToRadio &&to) override;
     meshtastic_FromRadio receive(void) override;
     virtual ~EthClient();
 
   protected:
     // low-level send method to write the encoded buffer to ethernet
-    virtual bool send(const uint8_t *buf, size_t len);
+    bool send(const uint8_t *buf, size_t len) override;
 
     // low-level receive method, periodically being called via thread
-    virtual size_t receive(uint8_t *buf, size_t space_left);
-
-    // received a full packet from ethernet, process it
-    virtual void handlePacketReceived(void);
-
-    // send packet via ethernet
-    virtual void handleSendPacket(void);
-
-    // thread handling stuff and data
-    static void task_loop(void *);
-    static EthClient *instance;
-
-    volatile bool shutdown;
-    uint8_t *buffer;
-
-    // local data
-    volatile bool connected;
-    size_t pb_size;
-    size_t bytes_read;
+    size_t receive(uint8_t *buf, size_t space_left) override;
 
     Client *client;
     uint8_t mac[6];
@@ -56,7 +37,4 @@ class EthClient : public IClientBase
     IPAddress serverIP;
     const char *server;
     uint16_t serverPort;
-
-    // receiver and sender queue
-    SharedQueue queue;
 };
--- source/comms/ethernet/EthClient.cpp
+++ source/comms/ethernet/EthClient.cpp
@@ -1,4 +1,5 @@
 #include "comms/EthClient.h"
+#include "comms/MeshEnvelope.h"
 #include "util/ILog.h"
 #include <time.h>
 
@@ -25,38 +26,22 @@ class EthernetClient : public WiFiClient
 #include "EthernetClient.h"
 #endif
 
-const size_t PB_BUFSIZE = 512 + 4; // 4 bytes for the header
+extern const uint8_t MT_MAGIC_0;
 
-EthClient *EthClient::instance = nullptr;
-
-EthClient::EthClient(const char *serverName, uint16_t port)
-    : shutdown(false), connected(false), pb_size(0), bytes_read(0), client(nullptr), mac{}, server(serverName), serverPort(port)
-{
-    // create a buffer for the incoming data
-    buffer = new uint8_t[PB_BUFSIZE];
-    instance = this;
-}
+EthClient::EthClient(const char *serverName, uint16_t port) : client(nullptr), mac{}, server(serverName), serverPort(port) {}
 
 EthClient::EthClient(IPAddress server, uint16_t port)
-    : shutdown(false), connected(false), pb_size(0), bytes_read(0), client(nullptr), mac{}, serverIP(server), server(nullptr),
-      serverPort(port)
+    : client(nullptr), mac{}, serverIP(server), server(nullptr), serverPort(port)
 {
-    // create a buffer for the incoming data
-    buffer = new uint8_t[PB_BUFSIZE];
-    instance = this;
 }
 
 EthClient::EthClient(uint8_t *mac, IPAddress ip, IPAddress server, uint16_t port)
-    : shutdown(false), connected(false), pb_size(0), bytes_read(0), client(nullptr), localIP(ip), serverIP(server),
-      server(nullptr), serverPort(port)
+    : client(nullptr), localIP(ip), serverIP(server), server(nullptr), serverPort(port)
 {
     memcpy(this->mac, mac, sizeof(this->mac));
     ILOG_DEBUG("EthClient mac=%02X:%02X:%02X:%02X:%02X:%02X localIP=%d.%d.%d.%d, serverIP=%d.%d.%d.%d", mac[0], mac[1], mac[2],
                mac[3], mac[4], mac[5], localIP[0], localIP[1], localIP[2], localIP[3], serverIP[0], serverIP[1], serverIP[2],
                serverIP[3]);
-    // create a buffer for the incoming data
-    buffer = new uint8_t[PB_BUFSIZE];
-    instance = this;
 }
 
 void EthClient::init(void)
@@ -75,48 +60,50 @@ void EthClient::init(void)
     if (Ethernet.linkStatus() == LinkOFF) {
         ILOG_WARN("Ethernet cable not connected!");
     }
-#elif defined(ARCH_PORTDUINO) || defined(HAS_WIFI)
+#elif defined(ARCH_PORTDUINO)
     client = new EthernetClient();
+#elif defined(HAS_WIFI)
+    // client = new WiFiClient();
     // WiFi.begin(ssid);
     // if (WiFi.status() != WL_CONNECTED) {
     //     ILOG_ERROR("WiFi/Eth device not found!");
     // }
 #endif
-
-#if defined(HAS_FREE_RTOS) || defined(ARCH_ESP32)
-    xTaskCreateUniversal(task_loop, "serial", 4096, NULL, 2, NULL, 0);
-#elif defined(ARCH_PORTDUINO)
-    new std::thread([] { instance->task_loop(nullptr); });
-#else
-// #error "unsupported architecture"
-#endif
-}
-
-bool EthClient::sleep(int16_t pin)
-{
-    return false;
+    SerialClient::init();
 }
 
 bool EthClient::connect(void)
 {
-    ILOG_INFO("EthClient connecting to %d.%d.%d.%d:%d ...", serverIP[0], serverIP[1], serverIP[2], serverIP[3], serverPort);
-    if (server != nullptr) {
-        connected = client->connect(server, SERVER_PORT);
-    } else {
-        connected = client->connect(serverIP, SERVER_PORT);
-    }
-    if (connected) {
-        ILOG_TRACE("EthClient connected!");
-    } else {
-        ILOG_WARN("EthClient connection failed!");
+    if (!connected) {
+        ILOG_INFO("EthClient connecting to %d.%d.%d.%d:%d ...", serverIP[0], serverIP[1], serverIP[2], serverIP[3], serverPort);
+        if (server != nullptr) {
+            connected = client->connect(server, SERVER_PORT);
+        } else {
+            connected = client->connect(serverIP, SERVER_PORT);
+        }
+        if (connected) {
+            ILOG_TRACE("EthClient connected!");
+
+            // skip some available bytes until we see magic header
+            int skipped = 0;
+            while (client->available()) {
+                if (client->peek() != MT_MAGIC_0) {
+                    client->read();
+                    skipped++;
+                    delay(1);
+                }
+            }
+            ILOG_TRACE("EthClient::connect skipped %d bytes", skipped);
+        } else {
+            ILOG_WARN("EthClient connection failed!");
+        }
     }
     return connected;
 }
 
 bool EthClient::disconnect(void)
 {
-    connected = false;
-    return connected;
+    return SerialClient::disconnect();
 }
 
 bool EthClient::isConnected(void)
@@ -125,29 +112,14 @@ bool EthClient::isConnected(void)
     return connected;
 }
 
-bool EthClient::send(meshtastic_ToRadio &&to)
-{
-    static uint32_t id = 1;
-    ILOG_TRACE("EthClient::send() push packet %d to queue", id);
-    queue.clientSend(DataPacket<meshtastic_ToRadio>(id++, to));
-    return false;
-}
-
 meshtastic_FromRadio EthClient::receive(void)
 {
-    if (queue.serverQueueSize() != 0) {
-        ILOG_TRACE("EthClient::receive() got a packet from queue");
-        auto p = queue.clientReceive()->move();
-        return static_cast<DataPacket<meshtastic_FromRadio> *>(p.get())->getData();
-    }
-    return meshtastic_FromRadio();
+    return SerialClient::receive();
 }
 
 EthClient::~EthClient()
 {
-    shutdown = true;
     delete client;
-    delete[] buffer;
 };
 
 // --- protected part ---
@@ -165,67 +137,36 @@ bool EthClient::send(const uint8_t *buf, size_t len)
 
 size_t EthClient::receive(uint8_t *buf, size_t space_left)
 {
-    // TODO: handle buffer overflow
-    size_t bytes_read = client->read(buf, space_left);
+#if 1
+    int bytes_read = 0;
+    while (client->available()) {
+        uint8_t byte = client->read();
+        *buf++ = byte;
+        if (++bytes_read >= space_left) {
+            ILOG_ERROR("buffer overflow! (%d too small)", space_left);
+            break;
+        }
+        if (client->peek() == MT_MAGIC_0)
+            break; // stop reading if we (potentially!) see next magic header
+    }
     if (bytes_read > 0) {
-        ILOG_TRACE("received %d bytes from radio", bytes_read);
-        return bytes_read;
+        ILOG_TRACE("received %d bytes from serial", bytes_read);
     }
     return bytes_read;
-}
-
-/**
- * @brief put received bytes into envelope and send to client queue
- *
- */
-void EthClient::handlePacketReceived(void)
-{
-    ILOG_TRACE("EthClient::handlePacketReceived pb_size=%d", pb_size);
-
-    //    MeshEnvelope envelope(buffer, pb_size);
-    //    meshtastic_FromRadio fromRadio = envelope.decode();
-    //    if (fromRadio.which_payload_variant != 0) {
-    //        queue.serverSend(DataPacket<meshtastic_FromRadio>(fromRadio.id, fromRadio));
-    //    }
-}
-
-void EthClient::handleSendPacket(void)
-{
-    auto p = queue.serverReceive()->move();
-    meshtastic_ToRadio toRadio = static_cast<DataPacket<meshtastic_ToRadio> *>(p.get())->getData();
-    //    MeshEnvelope envelope;
-    //    const std::vector<uint8_t> &pb_buf = envelope.encode(toRadio);
-    //    if (pb_buf.size() > 0) {
-    //        send(&pb_buf[0], pb_buf.size());
-    //    }
-}
-
-/**
- * @brief Sending and receiving packets to/from packet queue
- *
- */
-void EthClient::task_loop(void *)
-{
-    size_t space_left = PB_BUFSIZE - instance->pb_size;
-    ILOG_TRACE("EthClient::task_loop running");
-
-    while (!instance->shutdown) {
-        if (instance->connected) {
-            size_t bytes_read = instance->receive(&instance->buffer[instance->pb_size], space_left);
-            instance->pb_size += bytes_read;
-            size_t payload_len;
-            bool valid = true; // MeshEnvelope::validate(instance->buffer, instance->pb_size, payload_len);
-
-            if (valid) {
-                instance->handlePacketReceived();
-                //                MeshEnvelope::invalidate(instance->buffer, instance->pb_size, payload_len);
-            }
-
-            // send a packet if available
-            if (instance->queue.clientQueueSize() != 0) {
-                instance->handleSendPacket();
-            }
+#else // TODO: read all available bytes
+    int bytes_read = 0;
+    if (client->available()) {
+        bytes_read = client->read(buf, space_left);
+        if (bytes_read == 0) {
+            ILOG_TRACE("received %d bytes from radio", space_left);
+            return space_left;
+        } else if (bytes_read > 0) {
+            ILOG_TRACE("received %d bytes from radio", bytes_read);
+            return bytes_read;
+        } else {
+            return 0;
         }
-        delay(5);
     }
+    return bytes_read;
+#endif
 }
