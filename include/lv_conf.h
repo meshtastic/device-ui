34cdeee69323fbdae54d44c7f4670a5ae4efb234
Manuel <71137295+mverch67@users.noreply.github.com>
2026-01-23 00:24:46 +0100

    lodepng needs ARGB8888

--- include/lv_conf.h
+++ include/lv_conf.h
@@ -158,7 +158,7 @@
     #define LV_DRAW_SW_SUPPORT_RGB565A8      1
     #define LV_DRAW_SW_SUPPORT_RGB888        0
     #define LV_DRAW_SW_SUPPORT_XRGB8888      0
-    #define LV_DRAW_SW_SUPPORT_ARGB8888      0
+    #define LV_DRAW_SW_SUPPORT_ARGB8888      1
     #define LV_DRAW_SW_SUPPORT_L8            1
     #define LV_DRAW_SW_SUPPORT_AL88          1
     #define LV_DRAW_SW_SUPPORT_A8            1
