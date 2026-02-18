e12c80fadbe9aa924f59cdd4135f5b1bdf16f287
Manuel <71137295+mverch67@users.noreply.github.com>
2026-02-18 23:05:43 +0100

    add interface for X11 keyboard group

--- include/input/InputDriver.h
+++ include/input/InputDriver.h
@@ -40,6 +40,7 @@ class InputDriver
     virtual bool hasButtonDevice(void) { return button != nullptr; }
 
     virtual lv_indev_t *getKeyboard(void) { return keyboard; }
+    virtual void setKeyboard(lv_indev_t *kbd) { keyboard = kbd; }
     virtual lv_indev_t *getPointer(void) { return pointer; }
     virtual lv_indev_t *getEncoder(void) { return encoder; }
     virtual lv_indev_t *getButton(void) { return button; }
--- source/graphics/driver/X11Driver.cpp
+++ source/graphics/driver/X11Driver.cpp
@@ -3,6 +3,30 @@
 #include "util/ILog.h"
 #include <stdio.h>
 
+// note: copied from local lv_x11_input.c to access keyboard/mouse/wheel
+typedef struct _x11_inp_data {
+    /* LVGL related information */
+    lv_group_t *inp_group;    /**< input group for X input elements */
+    lv_indev_t *keyboard;     /**< keyboard input device object */
+    lv_indev_t *mousepointer; /**< mouse input device object */
+    lv_indev_t *mousewheel;   /**< encoder input device object */
+    lv_timer_t *timer;        /**< timer object for @ref x11_event_handler */
+    /* user input related information */
+    char kb_buffer[32];   /**< keyboard buffer for X keyboard inputs */
+    lv_point_t mouse_pos; /**< current reported mouse position */
+    bool left_mouse_btn;  /**< current state of left mouse button */
+    bool right_mouse_btn; /**< current state of right mouse button */
+    bool wheel_mouse_btn; /**< current state of wheel (=middle) mouse button */
+    int16_t wheel_cnt;    /**< mouse wheel increments */
+} x11_inp_data_t;
+
+static x11_inp_data_t *x11_input_get_user_data(lv_display_t *disp)
+{
+    _x11_user_hdr_t *disp_hdr = (_x11_user_hdr_t *)lv_display_get_driver_data(disp);
+    x11_inp_data_t **inp_data = &disp_hdr->inp_data;
+    return *inp_data;
+}
+
 LV_IMG_DECLARE(mouse_cursor_icon);
 
 X11Driver *X11Driver::x11driver = nullptr;
@@ -28,4 +52,27 @@ void X11Driver::init(DeviceGUI *gui)
     lv_x11_inputs_create(display, &mouse_cursor_icon);
 }
 
+lv_indev_t *X11Driver::getKeyboard(void)
+{
+    x11_inp_data_t *xd = x11_input_get_user_data(x11driver->display);
+    return xd->keyboard;
+}
+
+lv_indev_t *X11Driver::getPointer(void)
+{
+    x11_inp_data_t *xd = x11_input_get_user_data(x11driver->display);
+    return xd->mousepointer;
+}
+
+lv_indev_t *X11Driver::getEncoder(void)
+{
+    x11_inp_data_t *xd = x11_input_get_user_data(x11driver->display);
+    return xd->mousewheel;
+}
+
+lv_group_t *X11Driver::getGroup(void)
+{
+    x11_inp_data_t *xd = x11_input_get_user_data(x11driver->display);
+    return xd->inp_group;
+}
 #endif
