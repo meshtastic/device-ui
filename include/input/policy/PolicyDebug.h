bc1fbfdb7ea9b232687d7b7da31ab79e2cf806fd
mverch67 <manuel.verch@gmx.de>
2026-08-09 23:20:56 +0200

    fix POLICY_DEBUG

--- include/input/policy/PolicyDebug.h
+++ include/input/policy/PolicyDebug.h
@@ -2,8 +2,9 @@
 
 #include "util/ILog.h"
 
+//#define MUI_POLICY_DEBUG
 #ifdef MUI_POLICY_DEBUG
-#define POLICY_DEBUG ILOG_DEBUG(__VA_ARGS__)
+#define POLICY_DEBUG(...) ILOG_DEBUG(__VA_ARGS__)
 #else
 #define POLICY_DEBUG(...)
 #endif
