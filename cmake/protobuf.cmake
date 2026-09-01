bf381197e120777308433d6647c4bc0e0c3e75e6
mverch67 <manuel.verch@gmx.de>
2026-09-01 14:24:51 +0200

    update protobufs for CI / cmake

--- cmake/protobuf.cmake
+++ cmake/protobuf.cmake
@@ -2,6 +2,6 @@ message(STATUS "Fetching protobufs ...")
 FetchContent_Declare(
     Protobuf
     GIT_REPOSITORY "https://github.com/meshtastic/protobufs"
-    GIT_TAG  v2.7.8
+    GIT_TAG  v2.8.0
 )
 FetchContent_MakeAvailable(Protobuf)
