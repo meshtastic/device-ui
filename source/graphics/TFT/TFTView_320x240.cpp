e0b203f4e7ecc379bda1ca320f7a7da1363abe96
mverch67 <manuel.verch@gmx.de>
2026-09-14 20:54:58 +0200

    fix result expression

--- source/graphics/TFT/TFTView_320x240.cpp
+++ source/graphics/TFT/TFTView_320x240.cpp
@@ -6539,10 +6539,10 @@ void TFTView_320x240::backup(uint32_t option)
         // Write keys to file
         if (fs->open(fullPath, "w")) {
             bool result = true;
-            result |= fs->printf("config:\n") > 0;
-            result |= fs->printf("  security:\n") > 0;
-            result |= fs->printf("      privateKey: base64:%s\n", pskToBase64(privkey.bytes, privkey.size).c_str()) > 0;
-            result |= fs->printf("      publicKey: base64:%s\n", pskToBase64(pubkey.bytes, pubkey.size).c_str()) > 0;
+            result &= fs->printf("config:\n") > 0;
+            result &= fs->printf("  security:\n") > 0;
+            result &= fs->printf("      privateKey: base64:%s\n", pskToBase64(privkey.bytes, privkey.size).c_str()) > 0;
+            result &= fs->printf("      publicKey: base64:%s\n", pskToBase64(pubkey.bytes, pubkey.size).c_str()) > 0;
             fs->close();
             written = result;
         } else {
