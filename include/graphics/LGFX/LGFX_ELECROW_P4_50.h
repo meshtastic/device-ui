49f9d5a64aaa1d680d4f1ea9a4788cbd8e18b2ab
mverch67 <manuel.verch@gmx.de>
2026-09-16 15:15:21 +0200

    missing #

--- include/graphics/LGFX/LGFX_ELECROW_P4_50.h
+++ include/graphics/LGFX/LGFX_ELECROW_P4_50.h
@@ -97,7 +97,7 @@ class LGFX_ELECROW_P4_50 : public lgfx::LGFX_Device
     const uint32_t screenWidth = 800;
     const uint32_t screenHeight = 480;
 
-if defined(USE_FULL_DOUBLE_BUFFER)
+#if defined(USE_FULL_DOUBLE_BUFFER)
     void *getFrameBuffer(uint8_t index) const { return _bus_instance.getFrameBuffer(index); }
     bool presentFrameBuffer(const void *frame_buffer) { return _bus_instance.presentFrameBuffer(frame_buffer); }
     void waitFrameBuffer(void) { _bus_instance.waitFrameBuffer(); }
